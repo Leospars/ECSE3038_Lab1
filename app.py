6f7ef7800b2708083702e545a7e6a9fc3029f3c6
leospars <leongayle018@gmail.com>
2025-01-30 18:43:40 -0500

    feat: Assess patients' temperature

--- app.py
+++ app.py
@@ -8,3 +8,26 @@ def parallel(resistors: list[int | float]) -> float:
 
 resistance = parallel([330, 1000, 2200])
 print(f"Resistance: {resistance:.2f} Ohms")
+
+# Question 2
+def temperature_check(temp: float, degrees: str) -> str:
+    if degrees == "F":
+        if temp < 95:
+            return "the patient is hypothermic"
+        elif temp > 104:
+            return "the patient is hyperthermic"
+        else:
+            return "the patient's temperature is normal"
+    elif degrees == "C":
+        if temp < 35:
+            return "the patient is hypothermic"
+        elif temp > 40:
+            return "the patient is hyperthermic"
+        else:
+            return "the patient's temperature is normal"
+    else:
+        return "Invalid input degrees must be either F or C"
+
+temperature_check(14, "C")
+temperature_check(37, "C")
+temperature_check(37, "F")
