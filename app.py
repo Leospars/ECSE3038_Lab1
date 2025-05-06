ad9f77801e3134131f4f5cc246ceebcc3db93bdc
leospars <leongayle018@gmail.com>
2025-05-06 13:48:26 -0500

    feat: Add missing question

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ resistance = parallel([330, 1000, 2200])
 print(f"Resistance: {resistance:.2f} Ohms")
 
 # Question 2
+def potential_divider(voltage, resistors: list[int | float]) -> list[float]:
+    vdrops = []
+    rtotal = sum(resistors)
+    for res in resistors:
+        vdrops.append(voltage * (res/ rtotal))
+        print(f"V_R{vdrops.index(vdrops[-1])}: {vdrops[-1]}")
+    return vdrops
+
+potential_divider(9, [3000, 1000])
+
+# Question 3
 def temperature_check(temp: float, degrees: str) -> str:
     if degrees == "F":
         if temp < 95:
